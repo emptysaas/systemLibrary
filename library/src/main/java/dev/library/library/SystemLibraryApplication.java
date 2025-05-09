1f162dc7fc11246ea74f7a38113cb763333e9c95
emptysaas <luiz.gus.ms@gmail.com>
2025-05-09 15:27:23 -0300

    first commit

---
+++ library/src/main/java/dev/library/library/SystemLibraryApplication.java
@@ -0,0 +1,13 @@
+package dev.library.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemLibraryApplication.class, args);
+	}
+
+}
---
+++ library/src/test/java/dev/library/library/SystemLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.library.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
